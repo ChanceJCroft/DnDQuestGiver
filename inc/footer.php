3b47dd7b963379cd1ffad8db4c08ba1a23bb8bf0
Chance Croft <66639666+ChanceJCroft@users.noreply.github.com>
2020-12-25 12:06:18 -0800

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+<p>&copy; 2020 Chance Croft</p>
+</footer>
+
+</div> <!---- /containter ---->
+    </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DnD Quests</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/superhero/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <nav>
+            <ul class="nav nav-pills pull-right">
+                <li role="presentation" class="active"><a href="frontpage.php">Home</a></li>
+                <li role="presentation"><a href="#">About</a></li>
+                <li role="presentation"><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <h3 class="text-muted">DnD Quest Ideas</h3>
+    </div>
